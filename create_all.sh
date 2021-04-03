42be0c4f26236dc3e213b3672b6029949ce43dd8
James Miller <jamesstewartmiller@gmail.com>
2021-04-03 20:39:02 +0100

    continuing to edit to make functional

--- create_all.sh
+++ create_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -e "********* WARNING *********\n\nYou must have created the correct directory structure before running this script.  Run the script create_directories.sh as root first to create the directories.\n\n Also, you must edit and complete the two files in the directory env_files, before running this script.\n"
+echo -e "********* WARNING *********\n\nYou must have created the correct directory structure before running this script.  Run the script create_directories.sh as root first to create the directories.\n\nAlso, you must run the script initial_provision.sh, which will call this script when it has finished installing the podman images.  And also, you must edit and complete the two files in the directory env_files, before running this script.\n\nFinally, you must make sure that the host machine you are using has port 80 upwards open.  To do this, edit the /etc/sysctl.conf or /etc/sysctl.d/99-sysctl.conf or similar and include net.ipv4.ip_unprivileged_port_start=80 so that port 80 and 443 will open for connection to the nginx pod(swag).  Then reload sysctl using the command 'sudo systctl --system'."
 
-echo Have you completed the files in the directory env_files (select a number)?
+echo -e "Are the podman images installed and have you completed the files in the directory env_files and have you edited syctl.conf and reloaded (select a number)?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) break;;
@@ -10,16 +10,18 @@ select yn in "Yes" "No"; do
     esac
 done
 
-
+read -p "Enter project name:" project_name
 cp ./env_files/scripts_env ./.env
 
 set -a
+PROJECT_NAME=$project_name
 source .env
 set +a
 
 cp ./env_files/settings_env /etc/opt/${PROJECT_NAME}/settings/.env
 
 podman pod create --name $POD_NAME -p $PORT1_DESCRIPTION -p $PORT2_DESCRIPTION
+
 
 #./run_django_cont.sh
 #./run_duckdns_cont.sh
--- create_directories_old.sh
+++ create_directories_old.sh
@@ -8,16 +8,8 @@ fi
 read -p 'Project name:' PROJECT_NAME
 read -p 'Path to code (the django_artisan folder where manage.py resides):' code_path
 read -p 'Absolute path to User home dir:' user_dir
-read -p 'User account name:' user 
+
 mkdir -p /etc/opt/${PROJECT_NAME}/settings
 mkdir -p /etc/opt/${PROJECT_NAME}/static_files
 mkdir -p ${user_dir}/${PROJECT_NAME}/logs
 ln -s ${code_path} /opt/${PROJECT_NAME}
-
-sudo chown $user:$user /etc/opt/${PROJECT_NAME}
-sudo chown $user:$user /etc/opt/${PROJECT_NAME}/settings
-sudo chown $user:$user /etc/opt/${PROJECT_NAME}/static_files
-sudo chown $user:$user ${user_dir}/${PROJECT_NAME}/logs
-
-sudo echo net.ipv4.ip_unprivileged_port_start=80 >> /etc/sysctl.conf
-sudo sysctl --system
