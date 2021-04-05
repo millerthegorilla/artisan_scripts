87bc88dd259ca9523a7ad916ef69450efc96966e
James Miller <jamesstewartmiller@gmail.com>
2021-04-05 14:57:37 +0100

    added tests for exist to initial_provision.sh

--- bob.sh
+++ bob.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-echo -e "remove logs dir or save logs and remove logs dir(choose a number)?"
-select yn in "Yes" "No" "Save"; do
-    case $yn in
-        Yes ) logs_remove=1; break;;
-        No ) logs_remove=0; break;;
-        Save ) logs_remove=2; break;;
-    esac
-done
-
-if [[ logs_remove -eq 2 ]]
+podman image exists python:latest
+if [[ ! $? -eq 0 ]]
 then
-	echo "yeah baby, yeah!"
+   echo -e "Yeah baby, yeah!"
 fi
--- initial_provision.sh
+++ initial_provision.sh
@@ -1,10 +1,42 @@
-podman pull docker.io/library/python:latest
-podman pull docker.io/library/elasticsearch:7.11.2
-podman pull docker.io/library/mariadb:latest
-podman pull docker.io/library/memcached:latest
-podman pull docker.io/mkodockx/docker-clamav:latest
-podman pull docker.io/linuxserver/duckdns:latest
-podman pull docker.io/linuxserver/swag:latest
-podman build --tag='python:django' -f='./dockerfiles/dockerfile_django'
+podman image exists python:latest
+if [[ ! $? -eq 0 ]]
+then
+	podman pull docker.io/library/python:latest
+fi
+podman image exists elasticsearch:7.11.2
+if [[ ! $? -eq 0 ]]
+then
+	podman pull docker.io/library/elasticsearch:7.11.2
+fi
+podman image exists mariadb:latest
+if [[ ! $? -eq 0 ]]
+then
+        podman pull docker.io/library/mariadb:latest
+fi
+podman image exists memcached:latest
+if [[ ! $? -eq 0 ]]
+then
+        podman pull docker.io/library/memcached:latest
+fi
+podman image exists docker-clamav:latest
+if [[ ! $? -eq 0 ]]
+then
+        podman pull docker.io/mkodockx/docker-clamav:latest
+fi
+podman image exists duckdns:latest
+if [[ ! $? -eq 0 ]]
+then
+        podman pull docker.io/linuxserver/duckdns:latest
+fi
+podman image exists swag:latest
+if [[ ! $? -eq 0 ]]
+then
+        podman pull docker.io/linuxserver/swag:latest
+fi
+podman image exists python:django
+if [[ ! $? -eq 0 ]]
+then
+        podman build --tag='python:django' -f='./dockerfiles/dockerfile_django'
+fi
 
 ./create_all.sh
