5ae76056b4d3f4856929cf152e87ef696d23f9b3
James Miller <jamesstewartmiller@gmail.com>
2021-04-05 15:02:33 +0100

    edited create all blurb

--- create_all.sh
+++ create_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -e "********* WARNING *********\n\nYou must have created the correct directory structure before running this script.  Run the script create_directories.sh as root first to create the directories.\n\nAlso, you must run the script initial_provision.sh, which will call this script when it has finished installing the podman images.  And also, you must edit and complete the two files in the directory templates/env_files/ and save them in the directory ./env_files, before running this script.\n\nFinally, you must make sure that the host machine you are using has port 80 upwards open.  To do this manually, edit the /etc/sysctl.conf or /etc/sysctl.d/99-sysctl.conf or similar and include net.ipv4.ip_unprivileged_port_start=80 so that port 80 and 443 will open for connection to the nginx pod(swag).  Then reload sysctl using the command 'sudo systctl --system'.  Alternatively, the script create_directories.sh will do this for you."
+echo -e "********* WARNING *********\n\nYou must have created the correct directory structure before running this script.  Run the script create_directories.sh as root first to create the directories, lower the ports open to rootless in sysctl and open the firewall ports.\n\nAlso, you must run the script initial_provision.sh, which will call this script when it has finished installing the podman images and building the django enabled image.  You can interrupt the script at any time and cleanup using the script cleanup.sh"
 
-echo -e "Are the podman images installed and have you completed the files in the directory templates/env_files/ and saved them in the ./env_files directory and have you edited syctl.conf and reloaded (select a number)?"
+echo -e "Are the podman images installed (select a number)?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) break;;
