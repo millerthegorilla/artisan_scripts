d5f4b8a4f8148bb208e16560fbf1af6b55bdb112
James Miller <jamesstewartmiller@gmail.com>
2021-06-27 18:25:02 +0100

    in progress

--- container_scripts/run_maria_cont.sh
+++ container_scripts/run_maria_cont.sh
@@ -3,7 +3,7 @@
 podman run -dit -e MARIADB_ROOT_PASSWORD='davebob' --name ${MARIA_CONT_NAME} --pod ${POD_NAME} ${MARIA_IMAGE}
  
 read -p "Enter your MYSQL_ROOT_PASSWORD : " mysql_root_password
-podman exec -e DB_HOST=${MARIADB_HOST} -e MYSQL_ROOT_PASSWORD=${mysql_root_password} -it ${MARIA_CONT_NAME} bash -c "mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e 'CREATE DATABASE ${DB_NAME} CHARSET utf8; grant all privileges on ${DB_NAME}.* TO ${DB_USER}@${DB_HOST} identified by ${DB_PASSWORD};'"
+podman exec -e MYSQL_ROOT_PASSWORD=${mysql_root_password} -e DB_NAME=${DB_NAME} -e DB_USER=${DB_USER} -e DB_HOST=${DB_HOST} -e DB_PASSWORD=${DB_PASSWORD} -it ${MARIA_CONT_NAME} bash -c "mysql -uroot -p\${MYSQL_ROOT_PASSWORD} -e \"CREATE DATABASE \${DB_NAME} CHARSET utf8; grant all privileges on \${DB_NAME}.* TO \${DB_USER}@\${DB_HOST} identified by '$\{DB_PASSWORD}';\""
 unset mysql_root_password
 # mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e 'CREATE DATABASE ${DB_NAME} CHARSET utf8; grant all privileges on ${DB_NAME}.* TO ${DB_USER}@${DB_HOST} identified by \'${DB_PASSWORD}\';
 
