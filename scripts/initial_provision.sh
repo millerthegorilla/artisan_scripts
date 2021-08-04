3e1d230244b0f1940433c6b1392eb01047167355
james miller <jamesstewartmiller@gmail.com>
2021-08-04 10:20:42 +0100

    debugging

--- scripts/initial_provision.sh
+++ scripts/initial_provision.sh
@@ -2,51 +2,9 @@
 
 source ${SCRIPTS_ROOT}/.proj
 
-runuser --login ${USER_NAME} -c "
-podman image exists python:latest
-if [[ ! $? -eq 0 ]]
-then
-	podman pull docker.io/library/python:latest &
-fi
-
-podman image exists elasticsearch:7.11.2
-if [[ ! $? -eq 0 ]]
-then
-	podman pull docker.io/library/elasticsearch:7.11.2 &
-fi
-
-podman image exists mariadb:10.5
-if [[ ! $? -eq 0 ]]
-then
-    podman pull docker.io/library/mariadb:10.5 &
-fi
-
-podman image exists redis:6.2.2-buster
-if [[ ! $? -eq 0 ]]
-then
-    podman pull docker.io/library/redis:6.2.2-buster &
-fi
-
-podman image exists docker-clamav:latest
-if [[ ! $? -eq 0 ]]
-then
-    podman pull docker.io/mkodockx/docker-clamav:latest &
-fi
-
-podman image exists duckdns:latest
-if [[ ! $? -eq 0 ]]
-then
-    podman pull docker.io/linuxserver/duckdns:latest &
-fi
-
-podman image exists swag:1.14.0
-if [[ ! $? -eq 0 ]]
-then
-    podman pull docker.io/linuxserver/swag:version-1.14.0 &
-fi
+SCRIPTS_ROOT=${SCRIPTS_ROOT} ./image_ack.sh
 
 wait
-"
 
 if [[ ${DEBUG} == "TRUE" ]]
 then
