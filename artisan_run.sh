ba8621eab0ec5ee45eb7dfdfd00496be3f4ed0a8
James Miller <jamesstewartmiller@gmail.com>
2022-02-03 19:03:39 +0000

    updated install command

--- artisan_run.sh
+++ artisan_run.sh
@@ -24,9 +24,9 @@ while (( "$#" )); do
   case "$1" in
     install)
       ## added this option to archive it.
-      find . -type d -exec chmod 0550 {} +
-      find . -type f -exec chmod 0440 {} +
-      chmod 0550 -R *.sh
+      find . -type d -exec chmod 0555 {} +
+      find . -type f -exec chmod 0444 {} +
+      find . -type f -name "*.sh" -exec chmod 0550 {}
       find .git -type d | xargs chmod 755
       find .git/objects -type f | xargs chmod 444
       find .git -type f | grep -v /objects/ | xargs chmod 644
