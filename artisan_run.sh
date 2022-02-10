027fb9876ebff2709199e9fd7084935a33f406ae
James Miller <jamesstewartmiller@gmail.com>
2022-02-10 21:46:53 +0000

    rewriting help

--- artisan_run.sh
+++ artisan_run.sh
@@ -279,6 +279,11 @@ while (( "$#" )); do
       runuser --login ${USER_NAME} -P -c "podman exec -e PROJECT_NAME=${PROJECT_NAME} -it ${DJANGO_CONT_NAME} bash -c \"chown artisan:artisan -R /opt/${PROJECT_NAME}&& find /opt/${PROJECT_NAME} -type d -exec chmod 0550 {} + && find /opt/${PROJECT_NAME} -type f -exec chmod 0440 {} +\""
       runuser --login ${USER_NAME} -P -c "podman exec -e PROJECT_NAME=${PROJECT_NAME} -it ${DJANGO_CONT_NAME} bash -c \"chown artisan:artisan -R /etc/opt/${PROJECT_NAME} && find /etc/opt/${PROJECT_NAME} -type f -exec chmod 0440 {} + && find /etc/opt/${PROJECT_NAME} -type d -exec chmod 0550 {} +\""
       ;;
+    pip)
+      shift;
+      COMMANDS="$*"
+      runuser --login ${USER_NAME} -P -c "podman exec -e PROJECT_NAME=${PROJECT_NAME} -it ${DJANGO_CONT_NAME} bash -c \"pip ${COMMANDS}\""
+      ;;
     help|-h|-?|--help)
       echo -e "$ artisan_run command   - where command is one of clean,
 create [ variables, directories, images, containers, systemd ],
