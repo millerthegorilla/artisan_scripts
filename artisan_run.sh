79ab45cdb30fd7567d6661fcd7aaa228865925ae
James Miller <jamesstewartmiller@gmail.com>
2022-03-09 14:03:50 +0000

    chmod maria.sh during artisan_run install

--- artisan_run.sh
+++ artisan_run.sh
@@ -29,6 +29,7 @@ while (( "$#" )); do
       find .git -type d | xargs chmod 755
       find .git/objects -type f | xargs chmod 444
       find .git -type f | grep -v /objects/ | xargs chmod 644
+      chmod 0444 templates/maria/maria.sh
       exit $?
       ;;
     uninstall)
