481f95e8fc6f90442da10afd2af6d7fe7bc90d82
James Miller <jamesstewartmiller@gmail.com>
2022-03-09 13:58:37 +0000

    debugging

--- scripts/get_variables.sh
+++ scripts/get_variables.sh
@@ -85,47 +85,48 @@ then
     done
 fi
 
-# if [[ ${DEBUG} == "TRUE" ]]
-# then
-#     echo -e 'mount app source code directories'
-#     select yn in "Yes" "No"; do
-#         case $yn in
-#             Yes ) MOUNT_SRC_CODE="TRUE"; break;;
-#             No ) MOUNT_SRC_CODE="FALSE"; break;;
-#         esac
-#     done
-#     if [[ ${MOUNT_SRC_CODE} == "TRUE" ]]
-#     then
-#         cd /
-#         until [[ -d "${SRC_CODE_PATH}" && ! -L "${SRC_CODE_PATH}" ]] 
-#         do
-#             echo -e 'mount source code directories (1) or mount git directories (2)'
-#             select sg in "src" "git"; do
-#                 case $sg in
-#                     src ) MOUNT_GIT="FALSE"; break;;
-#                     git ) MOUNT_GIT="TRUE"; break;;
-#                 esac
-#             done
-#             if [[ ${MOUNT_GIT} == "TRUE" ]]
-#             then
-#                 SMSG='Symlinks will be to the git repository to allow you to use git submodules to track your code changes.'
-#             else
-#                 SMSG='Symlinks will be to the source code directories inside the git repository.  You will have to manually track source code changes, updating each git in each repository.'
-#             fi
-#             echo -e 'Absolute path to git repository (the folder where your app directories reside) - *IMPORTANT* There must only be git repository directories at this path, ie each subdirectory of this path must be of the form "app_name" which must be a git repository for your app, and must have the subdirectory "app_name" containing the django_source_code.'
-#             echo -e ${SMSG}
-#             read -p ":" -e SRC_CODE_PATH
-#             if [[ ! -d "${SRC_CODE_PATH}" ]]
-#             then
-#                echo -e "That path doesn't exist!"
-#             fi
-#             if [[ -L "${SRC_CODE_PATH}" ]]
-#             then
-#                 echo -e "Code path must not be a symbolic link"
-#             fi
-#         done
-#     fi
-# fi
+if [[ ${DEBUG} == "TRUE" ]]
+then
+    echo -e 'mount app source code directories'
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) MOUNT_SRC_CODE="TRUE"; break;;
+            No ) MOUNT_SRC_CODE="FALSE"; break;;
+        esac
+    done
+    if [[ ${MOUNT_SRC_CODE} == "TRUE" ]]
+    then
+        cd /
+        until [[ -d "${SRC_CODE_PATH}" && ! -L "${SRC_CODE_PATH}" ]] 
+        do
+            echo -e 'mount source code directories (1) or mount git directories (2)'
+            select sg in "src" "git"; do
+                case $sg in
+                    src ) MOUNT_GIT="FALSE"; break;;
+                    git ) MOUNT_GIT="TRUE"; break;;
+                esac
+            done
+            if [[ ${MOUNT_GIT} == "TRUE" ]]
+            then
+                SMSG='Symlinks will be to the git repository to allow you to use git submodules to track your code changes.'
+            else
+                SMSG='Symlinks will be to the source code directories inside the git repository.  You will have to manually track source code changes, updating each git in each repository.'
+            fi
+            echo -e 'Absolute path to git repository (the folder where your app directories reside) - *IMPORTANT* There must only be git repository directories at this path, ie each subdirectory of this path must be of the form "app_name" which must be a git repository for your app, and must have the subdirectory "app_name" containing the django_source_code.'
+            echo -e ${SMSG}
+            read -p ":" -e SRC_CODE_PATH
+            if [[ ! -d "${SRC_CODE_PATH}" ]]
+            then
+               echo -e "That path doesn't exist!"
+            fi
+            if [[ -L "${SRC_CODE_PATH}" ]]
+            then
+                echo -e "Code path must not be a symbolic link"
+            fi
+        done
+    fi
+fi
+cd ${SCRIPTS_ROOT}
 
 if [[ ${DEBUG} == "TRUE" && $(id -u ${USER_NAME}) -lt 1000 ]]
 then
@@ -282,7 +283,6 @@ echo -n $db_password | runuser --login "${USER_NAME}" -c "podman secret create \
 
 
 # variables for create_directories.sh
-echo $(whoami)
 echo PROJECT_NAME=${PROJECT_NAME} > .proj
 echo USER_NAME=${USER_NAME} >> .proj
 echo USER_DIR=${USER_DIR} >> .proj
