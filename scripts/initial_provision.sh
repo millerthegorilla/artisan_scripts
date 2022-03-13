0c419bb44fda404c60bf4e0124b66ef3af5c9453
James Miller <jamesstewartmiller@gmail.com>
2022-03-13 14:35:16 +0000

    added postfix to maria image in initial_provision.sh

--- scripts/initial_provision.sh
+++ scripts/initial_provision.sh
@@ -87,14 +87,22 @@ function build_maria()
    fi
    cp ${SCRIPTS_ROOT}/dockerfiles/dockerfile_maria /home/${USER_NAME}/dockerfile_maria
    cp ${SCRIPTS_ROOT}/dockerfiles/maria.sh /home/${USER_NAME}/maria.sh
-   runuser --login ${USER_NAME} -c "podman build --tag='maria:artisan' -f='dockerfile_maria'"
+   runuser --login ${USER_NAME} -c "podman build --tag='maria:artisan_${$1}' -f='dockerfile_maria'"
    echo -e "DBNAME=${DB_NAME}" > ${SCRIPTS_ROOT}/.images/maria
    echo -e "DBUSER=${DB_USER}" >> ${SCRIPTS_ROOT}/.images/maria 
    echo -e "DBHOST=${DB_HOST}" >> ${SCRIPTS_ROOT}/.images/maria 
    rm /home/${USER_NAME}/dockerfile_maria /home/${USER_NAME}/maria.sh
 }
 
-runuser --login ${USER_NAME} -c "podman image exists maria:artisan"
+if [[ ${DEBUG} == "TRUE" ]]
+then
+    postfix="dev"
+else
+    postfix="prod"
+fi
+
+runuser --login ${USER_NAME} -c "podman image exists maria:artisan_${postfix}"
+
 if [[ $? -eq 0 ]]
 then
     if [[ -e ${SCRIPTS_ROOT}/.images/maria ]]
@@ -102,11 +110,12 @@ then
         source ${SCRIPTS_ROOT}/.images/maria
         if [[ ${DBNAME} != ${DB_NAME} || ${DBUSER} != ${DB_USER} || ${DBHOST} != ${DB_HOST} ]]
         then
-            build_maria
+            build_maria ${postfix}
         fi
+        # maria image doesn't need to be built
     else
-        build_maria
+        build_maria ${postfix}
     fi
 else
-    build_maria
+    build_maria ${postfix}
 fi
