8b974316c40ddadc8abb8ce7f7f78427235b8eb4
James Miller <jamesstewartmiller@gmail.com>
2022-03-14 18:13:26 +0000

    stopped clamd from auto run, and freshclam, and changed target of secret duckdnstoken in duckdns_cont

--- container_scripts/run_clamd_cont.sh
+++ container_scripts/run_clamd_cont.sh
@@ -11,5 +11,5 @@ echo -e "run_clamd_cont.sh"
 source ${SCRIPTS_ROOT}/.env
 source ${SCRIPTS_ROOT}/.proj
 
-runuser --login ${USER_NAME} -P -c "podman run -dit --pod ${POD_NAME} ${UPDATES} -e CLAMAV_NO_CLAMD=true -v clam_vol:/var/lib/clamav --name ${CLAM_CONT_NAME} ${CLAM_IMAGE}"
+runuser --login ${USER_NAME} -P -c "podman run -dit --pod ${POD_NAME} ${UPDATES} -e CLAMAV_NO_CLAMD=true -e CLAMAV_NO_FRESHCLAMD=true -v clam_vol:/var/lib/clamav --name ${CLAM_CONT_NAME} ${CLAM_IMAGE}"
 
