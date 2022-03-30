d5e1914c6060243a771214e3638342942e5f4f47
James Miller <jamesstewartmiller@gmail.com>
2022-03-30 17:45:35 +0100

    debugging

--- container_scripts/run_swag_cont.sh
+++ container_scripts/run_swag_cont.sh
@@ -30,7 +30,7 @@ if [[ ${TLDOMAIN} == "TRUE" ]]
 then
 	runuser --login ${USER_NAME} -P -c "podman run -dit --pod=${POD_NAME}  --secret=DUCKDNSTOKEN,type=env,target=DUCKDNSTOKEN --name=${SWAG_CONT_NAME} --cap-add=NET_ADMIN -e PUID=1000 -e PGID=1000 -e TZ=\"Europe/London\" -e URL=${DUCKDNS_SUBDOMAIN} -e ENAIL=${CERTBOT_EMAIL} -e VALIDATION=duckdns -e EXTRA_DOMAINS=${EXTRA_DOMAINS} -e STAGING=$staging -v ${SWAG_HOST_VOL_STATIC}:${SWAG_CONT_VOL_STATIC}:Z -v ${SWAG_VOL_NAME}:/config/:Z ${SWAG_IMAGE}"
 else
-	runuser --login ${USER_NAME} -P -c "podman run -dit --pod=${POD_NAME}  --secret=DUCKDNSTOKEN,type=env,target=DUCKDNSTOKEN --name=${SWAG_CONT_NAME} --cap-add=NET_ADMIN -e PUID=1000 -e PGID=1000 -e TZ=\"Europe/London\" -e URL=${DUCKDNS_SUBDOMAIN} -e EMAIL=${CERTBOT_EMAIL} -e VALIDATION=duckdns -e STAGING=$staging -v ${SWAG_HOST_VOL_STATIC}:${SWAG_CONT_VOL_STATIC}:Z -v ${SWAG_HOST_VOL_MEDIA}:${SWAG_CONT_VOL_MEDIA} -v ${SWAG_HOST_LOG_DIR}:${SWAG_CONT_LOG_DIR}:Z -v ${SWAG_VOL_NAME}:/config/:Z ${SWAG_IMAGE}"
+	runuser --login ${USER_NAME} -P -c "podman run -dit --pod=${POD_NAME}  --secret=DUCKDNSTOKEN,type=env,target=DUCKDNSTOKEN --name=${SWAG_CONT_NAME} --cap-add=NET_ADMIN -e PUID=1000 -e PGID=1000 -e TZ=\"Europe/London\" -e URL=${DUCKDNS_SUBDOMAIN} -e EMAIL=${CERTBOT_EMAIL} -e VALIDATION=duckdns -e STAGING=$staging -v ${SWAG_HOST_VOL_STATIC}:${SWAG_CONT_VOL_STATIC}:Z -v ${DJANGO_HOST_MEDIA_VOL}:${SWAG_CONT_VOL_MEDIA} -v ${SWAG_HOST_LOG_DIR}:${SWAG_CONT_LOG_DIR}:Z -v ${SWAG_VOL_NAME}:/config/:Z ${SWAG_IMAGE}"
 fi
     
 runuser --login ${USER_NAME} -P -c "podman exec -it ${SWAG_CONT_NAME} bash -c \"chown abc -R ${SWAG_CONT_VOL_STATIC}\""
