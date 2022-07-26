01bddd678543ad07428ca1432d5fa76c06dfde86
james miller <jamesstewartmiller@gmail.com>
2022-07-26 13:11:55 +0100

    debugging

--- scripts/get_variables.sh
+++ scripts/get_variables.sh
@@ -8,29 +8,29 @@ fi
 
 function get_variables_and_make_project_file()
 {
-    source ${CONTAINER_SCRIPTS_ROOT}/questions.sh
+    source ${CONTAINER_SCRIPTS_ROOT}/setup/utils/local_settings.sh
     
-    for container in $(ls -d ${CONTAINER_SCRIPTS_ROOT}/containers/*)
-    do
-        source ${container}/variables/questions.sh
-    done
     if [[ -f ${SCRIPTS_ROOT/.PROJECT_SETTINGS} ]]
     then
         if grep -q '[^[:space:]]' ${SCRIPTS_ROOT}/.PROJECT_SETTINGS;
         then
             echo "local .PROJECT_SETTINGS exists and is not empty"
             echo "Moving it to PROJECT_SETTINGS_OLD"
-            mv ${SCRIPTS_ROOT}/.PROJECT_SETTINGS ${SCRIPTS_ROOT}/PROJECT_SETTINGS_OLD
+            mv ${SCRIPTS_ROOT}/.PROJECT_SETTINGS ${SCRIPTS_ROOT}/settings_files/PROJECT_SETTINGS_OLD.$(date +%d-%m-%y_%T)
         fi
     fi
 
-    cd ${CONTAINER_SCRIPTS};
-    cat ${LOCAL_SETTINGS_FILE} > ${SCRIPTS_ROOT}/.PROJECT_SETTINGS
+    # root questions including questions shared by containers
+    source ${CONTAINER_SCRIPTS_ROOT}/questions.sh
+    local_settings_file=$(local_settings "${CONTAINER_SCRIPTS_ROOT}/questions.sh")
+    cat ${local_settings_file} > ${SCRIPTS_ROOT}/.PROJECT_SETTINGS
 
+    # container specific questions
     for container in $(ls -d ${CONTAINER_SCRIPTS_ROOT}/containers/*)
     do
-        cd ${container}/variables;
-        cat ${LOCAL_SETTINGS_FILE} >> ${SCRIPTS_ROOT}/.PROJECT_SETTINGS
+        source ${container}/variables/questions.sh
+        local_settings_file=$(local_settings "${container}/variables/questions.sh")
+        cat ${local_settings_file} > ${SCRIPTS_ROOT}/.PROJECT_SETTINGS
     done
 
     echo -e "Do you want to save your settings as a settings file? : "
@@ -43,10 +43,10 @@ function get_variables_and_make_project_file()
 
     if [[ SAVE_SETTINGS == "TRUE" ]]
     then
-        cp ${SCRIPTS_ROOT}/.PROJECT_SETTINGS PROJECT_SETTINGS.PROJECT_NAME.$(date +%d-%m-%y_%T)
+        cp ${SCRIPTS_ROOT}/.PROJECT_SETTINGS ${SCRIPTS_ROOT}/settings_files/PROJECT_SETTINGS.PROJECT_NAME.$(date +%d-%m-%y_%T)
     fi
 
-    PROJECT_SETTINGS=${SCRIPTS_ROOT}/.PROJECT_SETTINGS
+    echo ${SCRIPTS_ROOT}/.PROJECT_SETTINGS
 }
 
 echo -e "Enter absolute filepath of project settings or press enter to accept default.\n \
@@ -61,7 +61,7 @@ elif [[ -n ${DEFAULT_PROJECT_FILE} && -f ${DEFAULT_PROJECT_FILE} ]];
 then
     PROJECT_SETTINGS=${DEFAULT_PROJECT_FILE}
 else
-    get_variables_and_make_project_file
+    PROJECT_SETTINGS=$(get_variables_and_make_project_file)
 fi
 
 -a
