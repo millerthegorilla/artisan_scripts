f1805c03b3ca81f7ae33dc2a9ae2e9d600dd07a0
james miller <jamesstewartmiller@gmail.com>
2022-07-26 15:55:50 +0100

    debugging

--- scripts/get_variables.sh
+++ scripts/get_variables.sh
@@ -22,7 +22,7 @@ function get_variables_and_make_project_file()
 
     # root questions including questions shared by containers
     local_settings_file=$(local_settings ${LOCAL_SETTINGS_FILE} "${CONTAINER_SCRIPTS_ROOT}/questions.sh"  | tee /dev/tty | tail -n 1 > /dev/null 2>&1)
-    echo $local_settings_file
+    echo debug 1 in get_variables 25 local_settings_file is $local_settings_file
     source ${CONTAINER_SCRIPTS_ROOT}/questions.sh  ${local_settings_file}
     cat ${local_settings_file} > ${SCRIPTS_ROOT}/.PROJECT_SETTINGS
 
