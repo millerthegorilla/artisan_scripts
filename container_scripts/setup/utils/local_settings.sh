1612537e73792e21f6eeee96e36b8d4170c71416
james miller <jamesstewartmiller@gmail.com>
2022-07-26 09:17:43 +0100

    debugging

--- container_scripts/setup/utils/local_settings.sh
+++ container_scripts/setup/utils/local_settings.sh
@@ -3,10 +3,10 @@
 EXT=${1}
 
 function local_settings()
-{
+{   # check if absolute or relative path
 	if [[ ${1} == /* ]]; then
-	      echo ${1}
-	   else
-	      echo $(dirname $(realpath ${EXT}))/${1}
-	   fi
+	   echo ${1}
+	else
+	  echo $(realpath ${EXT})/${1}
+	fi
 }
