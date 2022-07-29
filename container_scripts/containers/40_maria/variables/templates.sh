4362ca78718af2309a642f30906900af957e3665
james miller <jamesstewartmiller@gmail.com>
2022-07-29 22:36:24 +0100

    debugging

--- container_scripts/containers/40_maria/variables/templates.sh
+++ container_scripts/containers/40_maria/variables/templates.sh
@@ -2,6 +2,8 @@
 
 source ${PROJECT_SETTINGS}
 
+echo ${CURRENT_DIR}
+
 if [[ ${DEBUG} == "TRUE" ]]
 then
     cat ${CURRENT_DIR}/templates/maria_dev.sh | envsubst '$db_user:$db_host:$db_name' > ${CURRENT_DIR}/dockerfiles/maria.sh
