d6082451fd1d39563d6fdc81fc34e873b9941f94
james miller <jamesstewartmiller@gmail.com>
2022-07-30 11:07:30 +0100

    debugging

--- container_scripts/setup/utils/get_tag.sh
+++ container_scripts/setup/utils/get_tag.sh
@@ -3,7 +3,7 @@
 function get_tag()
 {
 	echo debug 1 get_tag custom_tag= ${CUSTOM_TAG}
-	source ${1}/../image/source.sh
+	source ${1}/source.sh
 	if [[ -n "${CUSTOM_TAG}" ]];
 	then
 		tag=${CUSTOM_TAG}
