defea1f6f1b832af1f9ec5d9b9189c19244f50c7
james miller <jamesstewartmiller@gmail.com>
2022-07-30 16:37:05 +0100

    debugging

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -8,10 +8,11 @@ fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${SCRIPTS_ROOT}" )" &> /dev/null && pwd )
 OWNER_NAME=$(stat -c "%U" ${SCRIPT_DIR})
-find . | xargs chown ${OWNER_NAME}:${OWNER_NAME}
-find . -type d | xargs chmod 0775
-find . -type f | xargs chmod 0660
-find .git -type d | xargs chmod 755
-find .git/objects -type f | xargs chmod 664
-find .git -type f | grep -v /objects/ | xargs chmod 644
-chmod 0770 ./artisan_run.sh
+find ${SCRIPTS_ROOT} | xargs chown ${OWNER_NAME}:${OWNER_NAME}
+find ${SCRIPTS_ROOT} -type d | grep -v "settings_files" | xargs chmod 0775
+find ${SCRIPTS_ROOT} -type f | xargs chmod 0660
+find ${SCRIPTS_ROOT}/.git -type d | xargs chmod 755
+find ${SCRIPTS_ROOT}/.git/objects -type f | xargs chmod 664
+find ${SCRIPTS_ROOT}/.git -type f | grep -v /objects/ | xargs chmod 644
+chmod 0770 ${SCRIPTS_ROOT}/artisan_run.sh
+chown root:root ${SCRIPTS_ROOT}/settings_files
