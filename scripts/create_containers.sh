a9d562c6f74000b64b2630ea4f7fc073097aedb0
james miller <jamesstewartmiller@gmail.com>
2022-07-30 17:27:38 +0100

    debugging

---
+++ scripts/create_containers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]
+then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+source ${PROJECT_SETTINGS}
+
+function run_files()
+{
+    for file in $(find ${CONTAINER_SCRIPTS_ROOT}/containers -type f -name "${1}.sh" | sort)
+    do
+        /bin/bash "${file}"
+    done
+}
+
+echo debug 2 create_all.sh run_files =${RUN_FILES[@]}
+echo debug 3 create_all.sh CONTAINER_SCRIPTS_ROOT =${CONTAINER_SCRIPTS_ROOT}
+
+for file in ${RUN_FILES[@]}
+do
+    echo debug 1 create_all.sh file=${file}
+    run_files ${file}
+done
