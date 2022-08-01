3012ae7c3e08a1e92ccd80aa27bc34eb984049a9
james miller <jamesstewartmiller@gmail.com>
2022-08-01 09:44:09 +0100

    debugging

--- scripts/create_containers.sh
+++ scripts/create_containers.sh
@@ -8,17 +8,15 @@ fi
 
 source ${PROJECT_SETTINGS}
 
-function run_files()
-{
-	for container_file in $(find ${CONTAINER_SCRIPTS_ROOT}/containers -type f -name "${1}.sh" | sort)
-    do
-        /bin/bash "${container_file}"
-    done
-}
-
 IFS=',' read -r -a run_files <<< "${RUN_FILES}"
 
-for run_file in "${run_files[@]}"
+for container in $(ls -d ${CONTAINER_SCRIPTS_ROOT}/containers/*)
 do
-    run_files ${run_file}
+    for run_file in "${run_files[@]}"
+    do
+        if [[ -f "container/${run_file}.sh" ]]
+        do
+            bash "container/${run_file}.sh"
+        done
+    done
 done
