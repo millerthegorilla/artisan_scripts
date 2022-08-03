afb2d6f39a576ffbe608f23f016e7a334402752a
james miller <jamesstewartmiller@gmail.com>
2022-08-03 14:32:54 +0100

    debugging

--- container_scripts/containers/40_maria/image/source.sh
+++ container_scripts/containers/40_maria/image/source.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-SOURCE=docker.io/library/mariadb:10.5
-TAG=mariadb:10.5
+SOURCE=docker.io/library/mariadb:latest
+TAG=mariadb:latest
+#SOURCE=docker.io/library/mariadb:10.5
+#TAG=mariadb:10.5
 # CUSTOM_TAG=maria:artisan
