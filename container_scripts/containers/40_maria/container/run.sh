89b47d0dc3ff1756b13e844199ae5e56aa501e61
james miller <jamesstewartmiller@gmail.com>
2022-08-03 18:09:04 +0100

    debugging

--- container_scripts/containers/40_maria/container/run.sh
+++ container_scripts/containers/40_maria/container/run.sh
@@ -13,5 +13,6 @@ source ${CONTAINER_SCRIPTS_ROOT}/setup/utils/current_dir.sh
 
 tag=$(get_tag ${CURRENT_DIR})
 
-runuser --login ${USER_NAME} -P -c "podman run -dit --name \"${MARIA_CONT_NAME}\" -e MARIADB_ROOT_PASSWORD=${MARIADB_ROOT_PASSWORD} -e MARIADB_PASSWORD=${DB_PASSWORD} -e MARIADB_DATABASE=${DB_NAME} -e MARIADB_USER=${DB_USER} -v ${DB_VOL}:/var/lib/mysql:Z --pod \"${POD_NAME}\" ${tag}"
+runuser --login ${USER_NAME} -P -c "podman run -dit --name \"${MARIA_CONT_NAME}\" -e MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=TRUE -v ${DB_VOL}:/var/lib/mysql:Z --pod \"${POD_NAME}\" ${tag}"
+# runuser --login ${USER_NAME} -P -c "podman run -dit --name \"${MARIA_CONT_NAME}\" -e MARIADB_ROOT_PASSWORD=${MARIADB_ROOT_PASSWORD} -e MARIADB_PASSWORD=${DB_PASSWORD} -e MARIADB_DATABASE=${DB_NAME} -e MARIADB_USER=${DB_USER} -v ${DB_VOL}:/var/lib/mysql:Z --pod \"${POD_NAME}\" ${tag}"
 # runuser --login ${USER_NAME} -P -c "podman run -dit --name \"${MARIA_CONT_NAME}\" -e MARIADB_ROOT_PASSWORD=${MARIADB_ROOT_PASSWORD} -e MARIADB_PASSWORD=${DB_PASSWORD} -e echo MARIADB_DATABASE=${DB_NAME} -e MARIADB_USER=${DB_USER} -v ${DB_VOL}:/var/lib/mysql:Z --pod \"${POD_NAME}\" ${tag}"
--- container_scripts/containers/40_maria/image/source.sh
+++ container_scripts/containers/40_maria/image/source.sh
@@ -4,4 +4,4 @@ SOURCE=docker.io/library/mariadb:latest
 TAG=mariadb:latest
 #SOURCE=docker.io/library/mariadb:10.5
 #TAG=mariadb:10.5
-# CUSTOM_TAG=maria:artisan
+CUSTOM_TAG=maria:artisan
