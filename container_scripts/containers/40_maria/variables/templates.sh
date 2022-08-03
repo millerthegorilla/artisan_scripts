b31e7281cb70c30d5fedf59f1bf67042554ba458
james miller <jamesstewartmiller@gmail.com>
2022-08-03 22:36:41 +0100

    debugging

--- container_scripts/containers/40_maria/variables/templates.sh
+++ container_scripts/containers/40_maria/variables/templates.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
+set -a
 source ${PROJECT_SETTINGS}
+set +a
+
 source ${CONTAINER_SCRIPTS_ROOT}/setup/setup.sh
 source ${CONTAINER_SCRIPTS_ROOT}/setup/utils/current_dir.sh
 
 if [[ ${DEBUG} == "TRUE" ]]
 then
-     cp ${CURRENT_DIR}/templates/maria_dev.sh ${CURRENT_DIR}/../image/dockerfile/maria.sh
+     cat ${CURRENT_DIR}/templates/maria_dev.sh | envsubst > ${CURRENT_DIR}/../image/dockerfile/maria.sh
 else
-     cp ${CURRENT_DIR}/templates/maria_prod.sh ${CURRENT_DIR}/../image/dockerfile/maria.sh
+     cat ${CURRENT_DIR}/templates/maria_prod.sh | envsubst > ${CURRENT_DIR}/../image/dockerfile/maria.sh
 fi
