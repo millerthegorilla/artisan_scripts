93b7d71ff8d2aefa1ae2c54c2ad03a898013ee8c
james miller <jamesstewartmiller@gmail.com>
2022-08-03 16:22:55 +0100

    debugging

--- container_scripts/systemd/generate_units.sh
+++ container_scripts/systemd/generate_units.sh
@@ -7,10 +7,10 @@ source ${CONTAINER_SCRIPTS_ROOT}/setup/utils/current_dir.sh
 SYSTEMD_UNIT_DIR="${CURRENT_DIR}/unit_files"
 
 # POD MUST BE RUNNING WITH ALL CONTAINERS RUNNING INSIDE
-runuser --login "${USER_NAME}" -c "mkdir ~/systemd; cd ~/systemd && podman generate systemd --new --name --files ${POD_NAME}"
+runuser --login "${USER_NAME}" -c "mkdir ${USER_DIR}/systemd; cd ${USER_DIR}/systemd && podman generate systemd --new --name --files ${POD_NAME}"
 
 # copy gitignore into empty dir
-cp ${CURRENT_DIR}/templates/systemd/systemd_git_ignore ${SYSTEMD_UNIT_DIR}/.gitignore
+cp ${CURRENT_DIR}/templates/systemd_git_ignore ${SYSTEMD_UNIT_DIR}/.gitignore
 cp ${USER_DIR}/systemd/* ${SYSTEMD_UNIT_DIR}
 chown -R ${USER}:${USER} ${SYSTEMD_UNIT_DIR}
 rm -rf ${USER_DIR}/systemd
--- container_scripts/systemd/templates.sh
+++ container_scripts/systemd/templates.sh
@@ -10,7 +10,7 @@ SYSTEMD_UNIT_DIR="${CURRENT_DIR}/unit_files"
 
 if [[ -d ${SYSTEMD_UNIT_DIR} ]];
 then
-	if [[ $(ls -A ${SYSTEMD_UNIT_DIR} ]];
+	if [[ $(ls -A ${SYSTEMD_UNIT_DIR}) ]];
 	then
 		echo -e "systemd unit_file directory is not empty.  Moving systemd unit_file directory."
 		mv ${SYSTEMD_UNIT_DIR} ${SYSTEMD_UNIT_DIR}.$(date +%d-%m-%y_%T)
@@ -22,13 +22,13 @@ if [[ ${DEBUG} == "TRUE" ]]
 then
 	if [[ $(systemctl get-default) == "graphical.target" && $(id -u ${USER_NAME}) -ge 1000 ]]
 	then 
-    	cat ${SCRIPTS_ROOT}/templates/systemd/manage_start_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/manage_start.service 
-    	cat ${SCRIPTS_ROOT}/templates/systemd/qcluster_start_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/qcluster_start.service 
+    	cat ${CURRENT_DIR}/templates/manage_start_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/manage_start.service 
+    	cat ${CURRENT_DIR}/templates/qcluster_start_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/qcluster_start.service 
     elif [[ $(systemctl get-default) == "graphical.target" && $(id -u ${USER_NAME}) -le 999 || $(systemctl get-default) == "multi-user.target" ]]
     then
-        cat ${SCRIPTS_ROOT}/templates/systemd/manage_start_non_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/manage_start.service
-    	cat ${SCRIPTS_ROOT}/templates/systemd/qcluster_start_non_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/qcluster_start.service
+        cat ${CURRENT_DIR}/templates/manage_start_non_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/manage_start.service
+    	cat ${CURRENT_DIR}/templates/qcluster_start_non_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/qcluster_start.service
     fi
 else
-    cat ${SCRIPTS_ROOT}/templates/systemd/qcluster_start_non_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/qcluster_start.service 
+    cat ${CURRENT_DIR}/templates/qcluster_start_non_graphical.service | envsubst > ${SYSTEMD_UNIT_DIR}/qcluster_start.service 
 fi
