40da759b73d0d758eaff09d9fbdb0ec299fd89f8
james miller <jamesstewartmiller@gmail.com>
2022-08-10 14:28:48 +0100

    debugging

--- container_scripts/containers/00_shared/variables/questions.sh
+++ container_scripts/containers/00_shared/variables/questions.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]
+then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 L_S_FILE=${1}
 
 echo -e "#******************************************************************"
--- container_scripts/containers/10_clamd/variables/questions.sh
+++ container_scripts/containers/10_clamd/variables/questions.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]
+then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 L_S_FILE=${1}
 
 source ${CONTAINER_SCRIPTS_ROOT}/containers/00_shared/variables/settings.sh
