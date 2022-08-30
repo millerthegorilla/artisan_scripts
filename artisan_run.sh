01388ea7afe654e3c901282636162def3241308c
james miller <jamesstewartmiller@gmail.com>
2022-08-30 15:43:14 +0100

    added create settings to artisan_run.sh

--- artisan_run.sh
+++ artisan_run.sh
@@ -22,6 +22,12 @@ then
     source ${PROJECT_SETTINGS}
 fi
 
+if [[ ! -d ${CURRENT_PROJECT_PATH}/settings_files ]];
+then
+    mkdir -p ${CURRENT_PROJECT_PATH}/settings_files/env_files;
+    chmod 0775 ${CURRENT_PROJECT_PATH}/settings_files ${CURRENT_PROJECT_PATH}/settings_files/env_files;
+fi
+
 function install_check()
 {
   INSTALLED="installed."
